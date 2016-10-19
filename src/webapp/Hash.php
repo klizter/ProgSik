18af351e33115c4165cd64145d137e53e10e752b
patfinzz <patrick.finseth@outlook.com>
2016-10-19 15:13:00 +0200

    Implemented new hash function and random salt

--- src/webapp/Hash.php
+++ src/webapp/Hash.php
@@ -5,19 +5,18 @@ namespace tdt4237\webapp;
 use Symfony\Component\Config\Definition\Exception\Exception;
 
 class Hash
-{
-
-    static $salt = "password";
-
+{  
+    static $salt = '';
 
     public function __construct()
     {
+        $salt = base64_encode(openssl_random_pseudo_bytes(30));
     }
 
     public static function make($plaintext)
     {
-        return hash('sha1', $plaintext . Hash::$salt);
-
+        
+        return hash('sha256', $plaintext . Hash::$salt);
     }
 
     public function check($plaintext, $hash)
